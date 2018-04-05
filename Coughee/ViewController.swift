1f56f1ba7b4c5284ef371804473d1bff7ea5e726
Melvin Hernandez <m.hernandez@berkeley.edu>
2018-04-04 19:09:57 -0700

    Initial commit.

---
+++ Coughee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coughee
+//
+//  Created by Melvin  Hernandez on 4/4/18.
+//  Copyright © 2018 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Coughee/Pods-Coughee-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coughee : NSObject
+@end
+@implementation PodsDummy_Pods_Coughee
+@end
