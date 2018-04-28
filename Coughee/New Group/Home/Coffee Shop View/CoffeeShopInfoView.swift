8cd084132dde96d20fd6ad41b30aee619a950d38
Melvin Hernandez <m.hernandez@berkeley.edu>
2018-04-28 14:03:12 -0700

    Updates to coffee shop info view.

--- Coughee/New Group/Home/Coffee Shop View/CoffeeShopInfoView.swift
+++ Coughee/New Group/Home/Coffee Shop View/CoffeeShopInfoView.swift
@@ -20,7 +20,7 @@ class CoffeeShopInfoView: BaseCollectionCell, UITableViewDelegate, UITableViewDa
     
     let tableView: UITableView = {
         let tv = UITableView()
-        tv.backgroundColor = Colors.gray
+        tv.backgroundColor = .white
         tv.separatorStyle = .none
         tv.separatorInset = .zero
         tv.translatesAutoresizingMaskIntoConstraints = false
@@ -41,7 +41,15 @@ class CoffeeShopInfoView: BaseCollectionCell, UITableViewDelegate, UITableViewDa
         ]
         tableView.register(MapCell.self, forCellReuseIdentifier: mapId)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.register(HoursCell.self, forCellReuseIdentifier: "hoursCell")
         NSLayoutConstraint.activate(tableConstraints)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        view.heightAnchor.constraint(equalToConstant: 40)
+        return view
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -50,21 +58,102 @@ class CoffeeShopInfoView: BaseCollectionCell, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 0:
-            return 1
+        case 2:
+            if let hours = coffeeShop?.hours.count {
+                return hours
+            }
+            return 0
         default:
             return 1
         }
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 1:
+            return "Rating"
+        case 2:
+            return "Hours"
+        case 3:
+            return "Address"
+        case 4:
+            return "Website"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        let header = UILabel()
+        header.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        header.textAlignment = .center
+        header.textColor = Colors.coral
+        header.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(header)
+        view.backgroundColor = UIColor(displayP3Red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
+        
+        let headerCons = [
+            header.topAnchor.constraint(equalTo: view.topAnchor),
+            header.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            header.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            header.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        
+        let viewCons = [
+            view.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+
+        ]
+        NSLayoutConstraint.activate(viewCons)
+        NSLayoutConstraint.activate(headerCons)
+        return view
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if (indexPath.section == 0 && indexPath.row == 0) {
+        switch indexPath.section {
+        case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: mapId, for: indexPath) as! MapCell
             cell.coffeeShop = self.coffeeShop
             cell.setupCell()
             return cell
-        } else {
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+            if let rating = coffeeShop?.rating {
+                cell.textLabel?.text = "\(rating)"
+            } else {
+                cell.textLabel?.text = "Unknown"
+            }
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "hoursCell", for: indexPath) as! HoursCell
+            if let hours = coffeeShop?.hours {
+                cell.hourLabel.text = hours[indexPath.row]
+            } else {
+                cell.hourLabel.text = "Unknown"
+            }
+            cell.setupHours()
+            return cell
+        case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+            if let addr = coffeeShop?.address {
+                cell.textLabel?.text = addr
+            } else {
+                cell.textLabel?.text = "Unknown"
+            }
+            return cell
+        case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+            if let website = coffeeShop?.website {
+                cell.textLabel?.text = website
+            } else {
+                cell.textLabel?.text = "Unknown"
+            }
+            return cell
+        default:
             let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
             cell.textLabel?.text = "Hello"
             return cell
@@ -72,13 +161,37 @@ class CoffeeShopInfoView: BaseCollectionCell, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if (indexPath.section == 0) {
+        switch indexPath.section {
+        case 0:
             return 250
-        } else {
-            return 70
+        case 2:
+            return 30
+        default:
+            return 40
         }
     }
     
+}
+
+class HoursCell: UITableViewCell {
+    let hourLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupHours() {
+        self.addSubview(hourLabel)
+        let textLabelCons = [
+            hourLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            hourLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            hourLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            hourLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ]
+        NSLayoutConstraint.activate(textLabelCons)
+    }
 }
 
 class MapCell: UITableViewCell {
