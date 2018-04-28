722ebba897e5120149cce6639ca8c0d73455d822
Ruben Escolero <rescolero@berkeley.edu>
2018-04-28 01:44:52 -0700

    Changes to User Profile

---
+++ Coughee/New Group/User Profile/UserBar.swift
@@ -0,0 +1,9 @@
+//
+//  UserBar.swift
+//  Coughee
+//
+//  Created by Ruben Escolero on 4/27/18.
+//  Copyright © 2018 UC Berkeley. All rights reserved.
+//
+
+import Foundation
--- Coughee/New Group/User Profile/UserViewController.swift
+++ Coughee/New Group/User Profile/UserViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import GoogleMaps
+import FontAwesome_swift
 
 class UserViewController: UIViewController {
+    
+    let currentUser = CurrentUser()
+    
+  
+    
+    let bottomContainer: UserContent = {
+        let container = UserContent()
+        return container
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Colors.gray
-        navigationItem.title = "Username here"
+        navigationItem.title = currentUser.username
+        setupContent()
+    }
+    
+    
+    func setupContent() {
+        bottomContainer.delegate = self
+        self.view.addSubview(bottomContainer)
+        bottomContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomContainer.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            bottomContainer.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            bottomContainer.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            bottomContainer.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+        
+    }
+    
+    @objc func createNewPost() {
+        let newPostView = CreatePostViewController()
+        self.present(newPostView, animated: true, completion: nil)
+    }
+    
+}
+
+extension UserViewController: UserNewPostDelegate {
+    
+    func showNewPostModal(menuItem: MenuItem) {
+        let createNewPost = CreatePostViewController()
+        createNewPost.menuItem = menuItem
+        self.present(createNewPost, animated: true, completion: nil)
     }
 }
